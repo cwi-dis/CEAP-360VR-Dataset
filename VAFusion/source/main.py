4ee1cea8d13c1c44ac2afd554fb951655b36c6b7
Tianyi Zhang <ufozty@126.com>
2020-07-26 20:25:20 +0200

    VAFusion
    End-to-end module for View-point-dependent Annotation Fusion for 360VR（VAFusion）

---
+++ VAFusion/source/main.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import numpy as np
+import VA_aligment
+import VA_clusters
+import VA_fusion
+import VA_util
+
+if __name__=="__main__":
+    samples =  pd.read_csv("../data/VA_HM_FrameData.csv")
+    samples_v = pd.read_csv("../VisualFeatureDataList.csv")
+    '''
+    time aligment
+    '''
+    shift_v,shift_a = VA_aligment.annotation_alignment(samples,samples_v)
+    #shift_v = np.load("../data/shift.npz")["shift_v"];shift_a = np.load("shift.npz")["shift_a"]
+    samples_shift = VA_aligment.alignment_users(samples,shift_v, shift_a)    
+    '''
+    clustering
+    '''
+    cluster_videos = VA_clusters.clustering_users(samples_shift)
+    '''
+    annotation fusion
+    '''
+    valence, arousal = VA_fusion.annotation_fusion(cluster_videos)
+    pitch,yaw,box =  VA_fusion.bounding_box_fusion(cluster_videos)
+    '''
+    save data
+    '''
+    np.savez("../results/fusion_result.npz",valence = valence, arousal = arousal, 
+             pitch= pitch, yaw = yaw, box = box )
+    np.savez("../results/shift.npz", shift_v= shift_v, shift_a = shift_a )
+    '''
+    draw result
+    '''
+    VA_util.draw_fusion_result(valence,arousal)
